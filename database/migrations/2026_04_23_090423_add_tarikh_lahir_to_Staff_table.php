1ed0a7f4591de372585d16b706e884254dc11f94
amri <selemann@melayu.com>
2026-04-23 17:04:23 +0800

    feat: add tarikh_lahir field to Staff

---
+++ database/migrations/2026_04_23_090423_add_tarikh_lahir_to_Staff_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('staff', function (Blueprint $table) {
+            $table->date('tarikh_lahir')->nullable(false);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('staff', function (Blueprint $table) {
+            $table->dropColumn('tarikh_lahir');
+        });
+    }
+};
