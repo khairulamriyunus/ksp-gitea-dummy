bc27ce7243d9caab2b2facec00f17bf1b37d0a42
amri <selemann@melayu.com>
2026-04-26 18:34:00 +0800

    chore: add resources/views/leaves/edit.blade.php

---
+++ resources/views/leaves/edit.blade.php
@@ -0,0 +1,16 @@
+<x-layouts.app title="{{ __('Leave') }}">
+    <form method="POST" action="{{ route('leaves.update', $record) }}" class="card">
+        @csrf
+        @method('PUT')
+        <div class="card-header">
+            <h3 class="card-title">{{ __('Leave') }}</h3>
+        </div>
+        <div class="card-body">
+
+        </div>
+        <div class="card-footer text-end">
+            <a href="{{ route('leaves.show', $record) }}" class="btn btn-link">{{ __('Cancel') }}</a>
+            <button type="submit" class="btn btn-primary">{{ __('Edit') }}</button>
+        </div>
+    </form>
+</x-layouts.app>
