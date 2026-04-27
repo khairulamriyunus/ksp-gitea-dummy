81df7d5f5ef45c791377af5b576bafd0f073227d
amri <selemann@melayu.com>
2026-04-27 23:01:09 +0800

    feat: update LeaveApplication model

---
+++ app/Models/LeaveApplication.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LeaveApplication extends Model
+{
+    protected $table = 'leave_applications';
+
+    protected $fillable = [
+        'applicant_name',
+    ];
+}
