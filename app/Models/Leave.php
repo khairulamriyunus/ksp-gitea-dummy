da01967585f252e55a14a38171661c25ec940154
amri <selemann@melayu.com>
2026-04-27 08:20:41 +0800

    feat: update Leave model

--- app/Models/Leave.php
+++ app/Models/Leave.php
@@ -3,35 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Spatie\Activitylog\LogOptions;
-use Spatie\Activitylog\Traits\LogsActivity;
 
 class Leave extends Model
 {
-    use SoftDeletes, LogsActivity;
-
-    // this is the table name
     protected $table = 'leaves';
 
     protected $fillable = [
-        'full_name',
-        'email',
-        'leave_type',
         'start_date',
-        'end_date',
-        'reason',
-        'attachment',
-        'status',
     ];
 
     protected $casts = [
         'start_date' => 'date',
-        'end_date' => 'date',
     ];
-
-    public function getActivitylogOptions(): LogOptions
-    {
-        return LogOptions::defaults()->logFillable();
-    }
 }
