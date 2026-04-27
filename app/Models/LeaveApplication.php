bb2323fa30c580f89074902e1759686b61b58724
amri <selemann@melayu.com>
2026-04-27 23:01:11 +0800

    feat: update LeaveApplication model

--- app/Models/LeaveApplication.php
+++ app/Models/LeaveApplication.php
@@ -11,5 +11,10 @@ class LeaveApplication extends Model
     protected $fillable = [
         'applicant_name',
         'leave_type',
+        'start_date',
+    ];
+
+    protected $casts = [
+        'start_date' => 'date',
     ];
 }
